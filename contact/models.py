3af14200ae8fd3ad23dd9fa6b3f9ae643b316c3a
Mayke Peres <mayke.subhue@gmail.com>
2025-08-31 13:52:37 -0300

    Congfigurando para subir o servidor, e as urls

--- contact/models.py
+++ contact/models.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -25,12 +30,17 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='pictures/%y/%m/')
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
-    
-    def __srt__(self) -> str:
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
