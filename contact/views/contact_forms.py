4ef439b1a57a7ef20b9b317366263aeda8c0c171
Mayke Peres <mayke.subhue@gmail.com>
2025-09-22 21:12:55 -0300

    Criei a parte de login do usuário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,29 +1,34 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+
 from contact.forms import ContactForm
 from contact.models import Contact
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES)
-        
+
         context = {
-                'form': form,
-                'form_action': form_action,
+            'form': form,
+            'form_action': form_action,
         }
 
         if form.is_valid():
-            contact = form.save()
-            return redirect("contact:update", contact_id=contact.pk)
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            return redirect('contact:update', contact_id=contact.pk)
 
         return render(
-                request,
-                'contact/create.html',
-                context
-            )
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
         'form': ContactForm(),
@@ -36,27 +41,31 @@ def create(request):
         context
     )
 
+
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES, instance=contact)
-        
+
         context = {
-                'form': form,
-                'form_action': form_action,
+            'form': form,
+            'form_action': form_action,
         }
 
         if form.is_valid():
             contact = form.save()
-            return redirect("contact:update", contact_id=contact.pk)
+            return redirect('contact:update', contact_id=contact.pk)
 
         return render(
-                request,
-                'contact/create.html',
-                context
-            )
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
         'form': ContactForm(instance=contact),
@@ -69,9 +78,11 @@ def update(request, contact_id):
         context
     )
 
+
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
 
