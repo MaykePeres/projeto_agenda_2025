2edea50a71fd1b8efa2cf803ffc5e8a66000e150
Mayke Peres <mayke.subhue@gmail.com>
2025-09-27 19:28:05 -0300

    finalizei os detalhesn do meu site

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 def register(request):
     form = RegisterForm()
@@ -25,32 +25,32 @@ def register(request):
     )
 
 
-# @login_required(login_url='contact:login')
-# def user_update(request):
-#     form = RegisterUpdateForm(instance=request.user)
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
 
-#     if request.method != 'POST':
-#         return render(
-#             request,
-#             'contact/user_update.html',
-#             {
-#                 'form': form
-#             }
-#         )
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
 
-#     form = RegisterUpdateForm(data=request.POST, instance=request.user)
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
 
-#     if not form.is_valid():
-#         return render(
-#             request,
-#             'contact/user_update.html',
-#             {
-#                 'form': form
-#             }
-#         )
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
 
-#     form.save()
-#     return redirect('contact:user_update')
+    form.save()
+    return redirect('contact:user_update')
 
 
 def login_view(request):
@@ -75,7 +75,7 @@ def login_view(request):
     )
 
 
-# @login_required(login_url='contact:login')
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
